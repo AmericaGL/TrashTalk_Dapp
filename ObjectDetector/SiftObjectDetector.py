54f600019958400726ea5e6824a704b639835c9c
AmericaGL <info@cybercodetwins.com>
2018-02-04 14:52:32 -0800

    object detection with opencv

---
+++ ObjectDetector/SiftObjectDetector.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+import calendar
+import time
+import urllib2
+import json
+
+MIN_MATCH_COUNT=30
+
+detector=cv2.xfeatures2d.SIFT_create()
+
+FLANN_INDEX_KDITREE=0
+flannParam=dict(algorithm=FLANN_INDEX_KDITREE,tree=5)
+flann=cv2.FlannBasedMatcher(flannParam,{})
+
+trainImg=cv2.imread("TrainingData/TrainImg1.jpg",0)
+trainKP,trainDesc=detector.detectAndCompute(trainImg,None)
+
+cam=cv2.VideoCapture(0)
+while True:
+    ret, QueryImgBGR=cam.read()
+    QueryImg=cv2.cvtColor(QueryImgBGR,cv2.COLOR_BGR2GRAY)
+    queryKP,queryDesc=detector.detectAndCompute(QueryImg,None)
+    matches=flann.knnMatch(queryDesc,trainDesc,k=2)
+
+    goodMatch=[]
+    for m,n in matches:
+        if(m.distance<0.75*n.distance):
+            goodMatch.append(m)
+
+    if(len(goodMatch)>MIN_MATCH_COUNT):
+        tp=[]
+        qp=[]
+        for m in goodMatch:
+            tp.append(trainKP[m.trainIdx].pt)
+            qp.append(queryKP[m.queryIdx].pt)
+        tp,qp=np.float32((tp,qp))
+        H,status=cv2.findHomography(tp,qp,cv2.RANSAC,3.0)
+        h,w=trainImg.shape
+        trainBorder=np.float32([[[0,0],[0,h-1],[w-1,h-1],[w-1,0]]])
+        queryBorder=cv2.perspectiveTransform(trainBorder,H)
+        cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
+
+	url='https://trashtalk-beta-2.firebaseio.com/readings.json'
+	postdata={
+		'Date':str(calendar.timegm(time.gmtime())),
+		'Address':str('TCOL3A-OSW3MN-T2FVQ5-72NSOC-VWMH6W-HJTXFW-KSDJ'),
+		'Status':str('finished'),
+		'Total':5.1
+	}
+
+	req= urllib2.Request(url)
+	req.add_header('Content-Type', 'application/json')
+	data = json.dumps(postdata)
+
+
+	response = urllib2.urlopen(req,data)
+
+
+    else:
+        print "Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT)
+    cv2.imshow('result',QueryImgBGR)
+    if cv2.waitKey(10)==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
+
